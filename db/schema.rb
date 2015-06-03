7fde210fa1ec915869c56864a72b9b06fcbddc97
Max Tedford <max.tedford@gmail.com>
2015-06-03 15:06:30 -0600

    added the comments functionality to the article show pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603191431) do
+ActiveRecord::Schema.define(version: 20150603205300) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20150603191431) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.string   "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
