65f4212fd816834c14f51e4e02d9ce5686468ab2
Max Tedford <max.tedford@gmail.com>
2015-06-03 13:57:29 -0600

    adding the article_params helper to create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,14 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
+    @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
