e893b2f1b6358c8905e630e2f11c9f2634d4151f
Max Tedford <max.tedford@gmail.com>
2015-06-03 20:57:03 -0600

    added paperclip gem image functionality and also stylesheet for application layout directory

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603214649) do
+ActiveRecord::Schema.define(version: 20150604023322) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
